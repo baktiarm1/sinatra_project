8a85be27fdbfd75b0ffd062329bdd0bd56760fc4
Miah, Baktiar <Baktiar.Miah@priceline.com>
2019-12-14 15:06:10 -0500

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,10 +18,24 @@ class ApplicationController < Sinatra::Base
 
   get '/signup' do
      if Helpers.is_logged_in?(session)
-         redirect to '/journal'
+         redirect to '/journal_entries'
      end
 
      erb :"/users/create_user"
+  end
+
+  post '/signup' do
+    params.each do |label, input|
+      if input.empty?
+        flash[:new_user_error] = "Please enter a value for #{label}"
+        redirect to '/signup'
+      end
+    end
+
+    user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+    session[:user_id] = user.id
+
+    redirect to '/journal_entries'
   end
 
   get '/login' do
@@ -32,5 +46,26 @@ class ApplicationController < Sinatra::Base
       erb :"/users/login"
     end
 
+
+    post '/login' do
+        user = User.find_by(:username => params["username"])
+
+        if user && user.authenticate(params[:password])
+          session[:user_id] = user.id
+          redirect to '/journal_entries'
+        else
+          flash[:login_error] = "Incorrect login. Please try again."
+          redirect to '/login'
+        end
+      end
+
+    get '/logout' do
+        if Helpers.is_logged_in?(session)
+          session.clear
+          redirect to '/login'
+        else
+          redirect to '/'
+        end
+      end
 
 end
