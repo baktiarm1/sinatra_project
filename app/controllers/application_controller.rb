fc796976fda04fa609c3a615e8d3f01cef4e7955
Miah, Baktiar <Baktiar.Miah@priceline.com>
2019-12-14 13:30:39 -0500

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,29 @@
 require './config/environment'
+require 'sinatra/base'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
-
+ enable :sessions
+ use Rack::Flash
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
-    enable :sessions
+   
     set :session_secret, 'baktiarmiah'
   end
 
-  get '/' do 
-   erb :index
+    get '/' do 
+        erb :index
+    end 
+
+  get '/signup' do 
+     if Helpers.is_logged_in?(session)
+         redirect to '/journal'
+     end 
+
+     erb :"/users/create_user"
   end 
+
+
 
 end
